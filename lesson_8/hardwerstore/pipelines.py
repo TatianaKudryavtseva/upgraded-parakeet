ff66c372fa9adbaa5438a7f05f2ea8f8183c1844
Tatiana <lexta@ist.ru>
2022-07-28 22:31:21 +0300

    добавлен обработчик характеристик товаров

---
+++ lesson_8/hardwerstore/pipelines.py
@@ -0,0 +1,65 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+import hashlib
+import scrapy
+from scrapy.utils.python import to_bytes
+from pymongo import MongoClient, errors
+from itemadapter import ItemAdapter
+from scrapy.pipelines.images import ImagesPipeline
+
+
+class HardwerstorePipeline:
+    def __init__(self):
+        client = MongoClient('mongodb://localhost:27017')
+        self.mongo_base = client.store2507
+
+    def process_item(self, item, spider):
+        item['_id'] = item['_id']
+        item['name'] = item['name']
+        item['price'] = item['price']
+        item['url'] = item['url']
+        item['photos'] = self.process_photos_list_url(item['photos'])
+        item['spec'] = self.process_spec_dict(item['spec_name'], item['spec_value'])
+
+        collection = self.mongo_base[spider.name]
+
+        try:
+            collection.insert_one(item)
+        except errors.DuplicateKeyError:
+            collection.replace_one({'_id': item['_id']}, item)
+        return item
+
+    @staticmethod
+    def process_photos_list_url(photos):
+        lst_photo = []
+        for photo in photos:
+            lst_photo.append(photo['path'])
+        return lst_photo
+
+    @staticmethod
+    def process_spec_dict(spec_name, spec_value):
+        spec_dict = dict(zip(spec_name, spec_value))
+        return spec_dict
+
+
+class HardwerstorePhotosPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        if item['photos']:
+            for img in item['photos']:
+                try:
+                    yield scrapy.Request(img)
+                except Exception as e:
+                    print(e)
+
+    def item_completed(self, results, item, info):
+        item['photos'] = [itm[1] for itm in results if itm[0]]
+        return item
+
+    def file_path(self, request, response=None, info=None, *, item=None):
+        image_guid = hashlib.sha1(to_bytes(request.url)).hexdigest()
+        return f'full/{item["name"]}/{image_guid}.jpg'
